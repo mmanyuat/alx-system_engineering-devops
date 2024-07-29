2940533cf719e5de49258738af55281e3fda69f3
mmanyuat201 <mmanyuat201@gmail.com>
2024-07-29 12:50:19 -0700

    taks0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,7 +4,7 @@
 import requests
 import sys
 
-if __name__=="__main__":
+if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
 
     employee_id = sys.argv[1]
@@ -18,7 +18,7 @@ if __name__=="__main__":
         if todo.get("completed") is True:
             completed.append(todo.get("title"))
 
-    print("Employee {} is done with tasks({}/{})".format(user.get("name"),
-        len(completed), len(todos)))
+    print("Employee {} is done with tasks({}/{})".format(
+        user.get("name"), len(completed), len(todos)))
     for complete in completed:
         print("\t {}".format(complete))
