58c9ef70bf448f3d4a4f8ca7df57edb9c73f41b4
mmanyuat <mmanyuat201@gmail.com>
2024-08-01 15:01:44 +0000

    config

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,70 +1,21 @@
-# manifest.pp
-# Ensure the Nginx package is installed
+#automating configuration with puppet
+
 package { 'nginx':
   ensure => installed,
 }
-
-# Ensure the Nginx service is running and enabled
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+file_line { 'install':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;,
+  line    => 'rewrite ^/redirect_me https://www.mbusa.com/en/vehicles/class/g-class/suv permanent;',
 }
 
-# Create the custom 404 error page
 file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Package['nginx'],
+  content  => 'Hello World',
 }
 
-# Create the custom 404 error page
-file { '/var/www/html/404.html':
-  ensure  => file,
-  content => "Ceci n'est pas une page",
-  require => Package['nginx'],
-}
-
-# Define the Nginx site configuration with a 301 redirect
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-}
-
-# Define the content of the Nginx site configuration
-file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/default.erb':
-  ensure  => file,
-  content => @("EOF")
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    server_name _;
-
-    location / {
-        root /var/www/html;
-        index index.html;
-        try_files \$uri \$uri/ =404;
-    }
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    error_page 404 /404.html;
-    location = /404.html {
-        internal;
-    }
-}
-| EOF
-}
-
-# Ensure the Nginx configuration is valid and restart the service
-exec { 'validate_nginx':
-  command     => '/usr/sbin/nginx -t && systemctl restart nginx',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-available/default'],
-  require     => Service['nginx'],
-}
+service{ 'nginx':
+  ensure => running;
+  require => package['nginx'],
+} 
 
