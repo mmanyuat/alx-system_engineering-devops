33d7ea35fd534ca42382fe27eb32716d20fb8db4
mmanyuat <mmanyuat201@gmail.com>
2024-08-13 13:53:51 +0000

    response

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,8 @@ def number_of_subscribers(subreddit):
     """Function to determine the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Custom User-Agent for educational purpose'}
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-        except Exception as e:
-            return 0
+    response = requests.get(url, headers=headers allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get(subscribers)
