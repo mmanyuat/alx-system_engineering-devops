0663403804f715fcf3aad6110ca4a73dd9c584b5
mmanyuat <mmanyuat201@gmail.com>
2024-08-13 13:58:36 +0000

    ans

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,9 @@ import requests
 def number_of_subscribers(subreddit):
     """Function to determine the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User-Agent for educational purpose'}
+    headers = {
+            'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'
+    }
     response = requests.get(url, headers=headers allow_redirects=False)
     if response.status_code == 404:
         return 0
