6e8d4288dd862919bfcc1194222b6da768bbcddb
mmanyuat <mmanyuat201@gmail.com>
2024-08-22 07:35:18 +0000

    ans

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # Enabling the user holberton to logon and change setting
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.cong",
+  command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.cong",
   path    => '/usr/local/bin/:/bin/'
 }
 
 # increase soft file limit
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/^holberton soft/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
